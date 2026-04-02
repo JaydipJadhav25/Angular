84d83912fd8880811ca52b15af1e11ad257f64fe
JaydipJadhav25 <jaydipdjadhav@gmail.com>
2026-04-02 21:56:54 +0530

    feat: Signals with Data Types | Arrays & Objects

--- src/app/signals/signals.ts
+++ src/app/signals/signals.ts
@@ -1,4 +1,13 @@
 import { Component, signal } from '@angular/core';
+import { single } from 'rxjs';
+
+
+
+interface IUser{
+name : string ,
+ age : number
+}
+
 
 @Component({
   selector: 'app-signals',
@@ -6,19 +15,34 @@ import { Component, signal } from '@angular/core';
   templateUrl: './signals.html',
   styleUrl: './signals.css',
 })
+
+
 export class Signals {
   val  = 10;
-  count = signal(20);
+  count = signal<number>(20);
+  name = signal<string>("jaydip");
+  isVerify = signal<boolean>(false);
+
+   rollNo = signal<number[]>([10 ,20 ,30, 40, 50]);
+
+   user = signal<IUser[]>([
+    {
+    name : "jay",
+    age : 20
+   }
+   ]);
+
+   directSet(){
+    this.rollNo.set([20 , 40 , 50]);
+   }
+
+   getVal(val : string){
+     this.rollNo.update(num => [...num , Number(val)]);
+   }
 
 
-  increment(){
-    this.count.update(c=>c+1);
-  }
-   decrement(){
-    this.count.update(c=>c-1);
-  }
 
-   reset(){
-    this.count.set(0);
-  }
+
+  
+
 }
